57316d2c17a3213612b68973daf5237d1e4cd886
Baraq Lipshitz <baraqlip@gmail.com>
2025-10-29 20:19:13 +0100

    add softmax kernel

---
+++ npbench/benchmarks/deep_learning/softmax/softmax_pytorch.py
@@ -0,0 +1,9 @@
+import torch
+
+@torch.compile
+def softmax(x):
+    tmp_max = torch.max(x, dim=-1, keepdim=True).values
+    tmp_max = tmp_max.detach() # stop gradient
+    x = torch.exp(x - tmp_max)
+    denominator = torch.sum(x, dim=-1, keepdim=True)
+    return x / denominator
