29263c0aae6a7239ae58450f2f1d59ae4e665cd7
laylaylo <yayladereleyla@gmail.com>
2025-11-11 14:56:55 +0100

    add torch impl for seidel_2d kernel

--- npbench/benchmarks/polybench/seidel_2d/seidel_2d_pytorch.py
+++ npbench/benchmarks/polybench/seidel_2d/seidel_2d_pytorch.py
@@ -3,13 +3,14 @@ import torch
 
 def kernel(TSTEPS, N, A: torch.Tensor) -> torch.Tensor:
 
+    A_res = A.clone() if A.requires_grad else A
     for t in range(0, TSTEPS - 1):
         for i in range(1, N - 1):
-            A[i, 1:-1] += (A[i - 1, :-2] + A[i - 1, 1:-1] + A[i - 1, 2:] +
-                           A[i, 2:] + A[i + 1, :-2] + A[i + 1, 1:-1] +
-                           A[i + 1, 2:])
+            A_res[i, 1:-1] += (A_res[i - 1, :-2] + A_res[i - 1, 1:-1] + A_res[i - 1, 2:] +
+                           A_res[i, 2:] + A_res[i + 1, :-2] + A_res[i + 1, 1:-1] +
+                           A_res[i + 1, 2:])
             for j in range(1, N - 1):
-                A[i, j] += A[i, j - 1]
-                A[i, j] /= 9.0
+                A_res[i, j] += A_res[i, j - 1]
+                A_res[i, j] /= 9.0
 
-    return A
+    return A_res
