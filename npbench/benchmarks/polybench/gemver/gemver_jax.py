cc05cd553fb880927a19bc119f0a9f1721372925
jonasmirlach <jonas.mirlach@gmail.com>
2025-11-16 11:39:02 +0100

    Fix in gemver jax and torch kernels

--- npbench/benchmarks/polybench/gemver/gemver_jax.py
+++ npbench/benchmarks/polybench/gemver/gemver_jax.py
@@ -4,9 +4,7 @@ import jax.numpy as jnp
 
 @jax.jit
 def kernel(alpha, beta, A, u1, v1, u2, v2, w, x, y, z):
-    
     A += jnp.outer(u1, v1) + jnp.outer(u2, v2)
     x += beta * y @ A + z
     w += alpha * A @ x
-
-    return A, x, w
+    return A, w, x
--- npbench/benchmarks/polybench/gemver/gemver_pytorch.py
+++ npbench/benchmarks/polybench/gemver/gemver_pytorch.py
@@ -6,4 +6,4 @@ def kernel(alpha, beta, A, u1, v1, u2, v2, w, x, y, z):
     updated_A = A + torch.outer(u1, v1) + torch.outer(u2, v2)
     updated_x = x + beta * torch.matmul(y, updated_A) + z
     updated_w = w + alpha * torch.matmul(updated_A, updated_x)
-    return updated_A, updated_x, updated_w
+    return updated_A, updated_w, updated_x
