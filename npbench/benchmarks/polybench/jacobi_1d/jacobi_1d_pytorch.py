051fd998a70da9e8693517e00808ec8c8cb68369
laylaylo <yayladereleyla@gmail.com>
2025-11-23 16:56:31 +0100

    implement jacobi_1d like lax.fori_loop

--- npbench/benchmarks/polybench/jacobi_1d/jacobi_1d_pytorch.py
+++ npbench/benchmarks/polybench/jacobi_1d/jacobi_1d_pytorch.py
@@ -1,12 +1,20 @@
 import torch
 from typing import Tuple
 
+# M dataset - without loop_body: 1988ms, clone in loop_body: 1014ms (JAX: 47ms)
+# paper dataset - without loop_body: 2672ms, clone in loop_body: 1350ms (JAX: 72ms)
 
 def kernel(
     TSTEPS: int, A: torch.Tensor, B: torch.Tensor
 ):
-    A_res, B_res = (A.clone(), B.clone()) if A.requires_grad else (A, B)
+    @torch.compile
+    def loop_body(A, B):
+        A = A.clone()
+        B = B.clone() 
+        B[1:-1] = 0.33333 * (A[:-2] + A[1:-1] + A[2:])
+        A[1:-1] = 0.33333 * (B[:-2] + B[1:-1] + B[2:])
+        return A, B
+
     for t in range(1, TSTEPS):
-        B_res[1:-1] = 0.33333 * (A_res[:-2] + A_res[1:-1] + A_res[2:])
-        A_res[1:-1] = 0.33333 * (B_res[:-2] + B_res[1:-1] + B_res[2:])
-    return A_res , B_res
+        A, B = loop_body(A, B)
+    return A , B
