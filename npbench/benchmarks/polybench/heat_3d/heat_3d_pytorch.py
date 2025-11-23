285efdc6eda1c9ef1c9e2d0784ea1cb819a2e017
laylaylo <yayladereleyla@gmail.com>
2025-11-23 16:30:26 +0100

    call .clone()  in func_body

--- npbench/benchmarks/polybench/heat_3d/heat_3d_pytorch.py
+++ npbench/benchmarks/polybench/heat_3d/heat_3d_pytorch.py
@@ -1,7 +1,12 @@
 import torch
 
+# M dataset - clone in kernel: 28ms, clone in func_body: 20ms
+# paper dataset - clone in kernel: 514ms, clone in func_body: 360ms
+
 @torch.compile
 def func_body(A, B):
+    A = A.clone()
+    B = B.clone()
     B[1:-1, 1:-1, 1:-1] = (
         0.125 * (A[2:, 1:-1, 1:-1] - 2.0 * A[1:-1, 1:-1, 1:-1] + A[:-2, 1:-1, 1:-1])
         + 0.125
@@ -18,16 +23,15 @@ def func_body(A, B):
         * (B[1:-1, 1:-1, 2:] - 2.0 * B[1:-1, 1:-1, 1:-1] + B[1:-1, 1:-1, 0:-2])
         + B[1:-1, 1:-1, 1:-1]
     )
-
-
+    return A, B
 
 
 def kernel(TSTEPS, A, B):
-    if A.requires_grad:
-        A = A.clone()
-    if B.requires_grad:
-        B = B.clone()
+    # if A.requires_grad:
+    #     A = A.clone()
+    # if B.requires_grad:
+    #     B = B.clone()
 
     for t in range(1, TSTEPS):
-        func_body(A, B)
+        A, B = func_body(A, B)
     return A, B
