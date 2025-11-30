3da53b27adf6c51337a6a4860b59e949123153a1
Baraq Lipshitz <baraqlip@gmail.com>
2025-11-30 19:00:32 +0100

    kernel working, not compiled

--- npbench/benchmarks/polybench/durbin/durbin_pytorch.py
+++ npbench/benchmarks/polybench/durbin/durbin_pytorch.py
@@ -5,12 +5,14 @@ def kernel(r):
 
     y = torch.empty_like(r)
     alpha = -r[0]
-    beta = 1.0
+    beta = torch.tensor([1.0], dtype=r.dtype, device=r.device)
     y[0] = -r[0]
 
     for k in range(1, r.shape[0]):
+        beta = beta.clone()
         beta *= 1.0 - alpha * alpha
         alpha = -(r[k] + torch.dot(torch.flip(r[:k], [0]), y[:k])) / beta
+        y = y.clone()
         y[:k] += alpha * torch.flip(y[:k], [0])
         y[k] = alpha
 
